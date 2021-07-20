99fcbfdf7d086bb9e5c78adb2b6479bf8b37758f
Joel Martinez <joemartinez87@hotmail.com>
2021-07-19 19:33:14 -0700

    routes api folder

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -1,11 +1,50 @@
 const router = require('express').Router()
-const db= require('')
+const db= require('../../models')
 
 // /api/workouts endpoint
 router.get('/', (req, res) => { 
-    
+    db.Workout.find()
+    .sort({day: -1}) 
+    .limit(1)
+    .then(workout => {
+        res.json(workout)
+    })
+    .catch(err => {
+        res.json(err)
+    }) 
 });
 
 
+router.post('/', (req, res) => { 
+    db.Workout.create(req.body)
+    .then(workout => {
+        res.json(workout)
+    })
+    .catch(err => {
+        res.json(err)
+    }) 
+});
+
+router.put('/:id', (req, res) => { 
+    db.Workout.findByIdAndUpdate(req.params.id, {$addToSet: {exercises: req.body}})
+    .then(workout => {
+        res.json(workout)
+    })
+    .catch(err => {
+        res.json(err)
+    }) 
+});
+
+router.get('/range', (req, res) => { 
+    db.Workout.find()
+    // .sort({day: 1}) 
+    .limit(7)
+    .then(workout => {
+        res.json(workout)
+    })
+    .catch(err => {
+        res.json(err)
+    }) 
+});
 
 module.exports = router
